a6969fb3dd31bf495286099c042af40489ec6868
Nicholas Cancelliere <ncancelliere@gmail.com>
2009-04-12 14:37:34 -0500

    first commit

---
+++ init.rb
@@ -0,0 +1,9 @@
+# Include hook code here
+require 'boiler_plate/validation_reflection'
+require 'field_set_form_builder'
+
+ActiveRecord::Base.class_eval do
+  include BoilerPlate::ActiveRecordExtensions::ValidationReflection
+  BoilerPlate::ActiveRecordExtensions::ValidationReflection.load_config
+  BoilerPlate::ActiveRecordExtensions::ValidationReflection.install(self)
+end
