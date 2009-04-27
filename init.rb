f36bbb7b729a81747b8e8536ddc1033f16b348de
Nicholas Cancelliere <ncancelliere@gmail.com>
2009-04-27 15:04:24 -0500

    added helper method fieldset_form

--- init.rb
+++ init.rb
@@ -1,6 +1,7 @@
 # Include hook code here
 require 'boiler_plate/validation_reflection'
 require 'field_set_form_builder'
+require 'field_set_form'
 
 ActiveRecord::Base.class_eval do
   include BoilerPlate::ActiveRecordExtensions::ValidationReflection
